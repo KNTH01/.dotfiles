29dd8efa613ee229f08172ef9bf42e1b1ad38733
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-11-14 00:11:57 +0100

    nvim: update null-ls

--- nvim/.config/nvim/lua/plugins/null_ls.lua
+++ nvim/.config/nvim/lua/plugins/null_ls.lua
@@ -1,32 +1,27 @@
 local null_ls = require("null-ls")
 
 local sources = {
-  null_ls.builtins.formatting.prettier.with({
-    filetypes = {
-      "javascript",
-      "typescript",
-      "css",
-      "scss",
-      "html",
-      "json",
-      "yaml",
-      "markdown",
-      "graphql",
-      "md",
-      "txt",
-    },
-  }),
-
+  null_ls.builtins.formatting.prettier,
+  null_ls.builtins.formatting.eslint_d,
+  null_ls.builtins.formatting.shfmt,
   null_ls.builtins.formatting.stylua.with({
     args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
   }),
+  null_ls.builtins.formatting.codespell,
+
+  null_ls.builtins.diagnostics.eslint_d,
+  null_ls.builtins.code_actions.gitsigns,
 }
 
 require("null-ls").config({
   sources = sources,
 })
 
-require("lspconfig")["null-ls"].setup({})
+require("plugins.lsp_config")
+require("lspconfig")["null-ls"].setup({
+  on_attach = On_attach,
+  capabilities = Capabilities,
+})
 
 -- the duration in there is to stop timeouts on massive files
 -- vim.cmd("autocmd BufWritePost * lua vim.lsp.buf.formatting_seq_sync(nil, 7500)")
