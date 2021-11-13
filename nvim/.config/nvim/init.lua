24ee7e48709e4c9257e7ae23eae78b4ae9747c7b
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-11-13 13:14:54 +0100

    nvim: move autosave config out

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -142,23 +142,7 @@ require("packer").startup(function()
   -- AutoSave
   use({
     "Pocco81/AutoSave.nvim",
-    config = function()
-      local autosave = require("autosave")
-
-      autosave.setup({
-        enabled = true,
-        execution_message = "autosaved at : " .. vim.fn.strftime("%H:%M:%S"),
-        events = { "InsertLeave", "TextChanged" },
-        conditions = {
-          exists = true,
-          filetype_is_not = {},
-          modifiable = true,
-        },
-        clean_command_line_interval = 2500,
-        on_off_commands = true,
-        write_all_buffers = false,
-      })
-    end,
+    config = [[ require('plugins/autosave') ]],
   })
 
   -- Colorizer
@@ -177,7 +161,6 @@ require("packer").startup(function()
 end)
 
 -------------
-
 
 -- Gitsigns
 require("gitsigns").setup({
