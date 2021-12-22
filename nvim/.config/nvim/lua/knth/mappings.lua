15a5b768dcd9637a83502245ce47efe902dcd663
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-12-22 13:08:40 +0100

    nvim: rework mappings

--- nvim/.config/nvim/lua/knth/mappings.lua
+++ nvim/.config/nvim/lua/knth/mappings.lua
@@ -1,48 +1,90 @@
 -- Mappings
 
-local map_opt = { noremap = true, silent = true }
+--   Modes
+--      normal_mode = "n",
+--      insert_mode = "i",
+--      visual_mode = "v",
+--      visual_block_mode = "x",
+--      term_mode = "t",
+--      command_mode = "c",
+
+local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 -- alias for keymap fn
 local keymap = vim.api.nvim_set_keymap
 
 -- remap space as leader key
-keymap("", "<Space>", "<Nop>", map_opt)
+keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
---Remap for dealing with word wrap
-keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
-keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
-
--- Y yank until the end of line
-keymap("n", "Y", "y$", map_opt)
-
--- set hlsearch to false
-keymap("n", "<esc><esc>", "<cmd>nohl<CR>", map_opt)
-keymap("n", "<leader>h", "<cmd>nohl<CR>", map_opt)
-
--- :w & :q cmds
-keymap("n", "<C-s>", "<cmd>w<CR>", map_opt)
-keymap("n", "<leader>w", "<cmd>w<CR>", map_opt)
-keymap("n", "<leader>q", "<cmd>q<CR>", map_opt)
-
--- copy current file path
-keymap("n", "<leader>cp", '<cmd>let @+ = expand("%")<CR>', map_opt)
-
--- replace the visual selection without pushing the latter into the register
--- #todo: seems not to work
-keymap("v", "<leader>p", "_P", map_opt)
+-- NORMAL
+--
 
 -- nvim config
 local nvim_config_path = "~/.config/nvim/init.lua"
-keymap("n", "<leader>vs", "<cmd>source " .. nvim_config_path .. "<cr>", map_opt)
-keymap("n", "<leader>ve", "<cmd>vsplit " .. nvim_config_path .. "<cr>", map_opt)
+keymap("n", "<leader>vs", "<cmd>source " .. nvim_config_path .. "<cr>", opts)
+keymap("n", "<leader>ve", "<cmd>vsplit " .. nvim_config_path .. "<cr>", opts)
 
--- easier moving of code blocks by keeping selection in visual mode
-keymap("v", "<", "<gv", map_opt)
-keymap("v", ">", ">gv", map_opt)
+-- Better window navigation
+-- keymap("n", "<C-h>", "<C-w>h", opts)
+-- keymap("n", "<C-j>", "<C-w>j", opts)
+-- keymap("n", "<C-k>", "<C-w>k", opts)
+-- keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- @TODO: close buffer?
+
+-- Remap for dealing with word wrap
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+-- set hlsearch to false
+keymap("n", "<esc><esc>", "<cmd>nohl<CR>", opts)
+keymap("n", "<leader>h", "<cmd>nohl<CR>", opts)
+
+-- :w & :q cmds
+keymap("n", "<C-s>", "<cmd>w<CR>", opts)
+keymap("n", "<leader>w", "<cmd>w<CR>", opts)
+keymap("n", "<leader>q", "<cmd>q<CR>", opts)
+
+-- copy current file path @TODO: create a vim command, cf formatting
+keymap("n", "<leader>cp", '<cmd>let @+ = expand("%")<CR>', opts)
 
 -- quickfix list navigation
-keymap("n", "<C-j>", ":cnext<cr>zz", map_opt)
-keymap("n", "<C-k>", ":cprev<cr>zz", map_opt)
+keymap("n", "<C-j>", ":cnext<cr>zz", opts)
+keymap("n", "<C-k>", ":cprev<cr>zz", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- VISUAL
+--
+
+-- replace the visual selection without pushing the latter into the register
+keymap("v", "p", '"_dP', opts)
+
+-- easier moving of code blocks by keeping selection in visual mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- VISUAL BLOCK
+--
+
+-- Move text up and down
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
