644286e13a9f527c9e4997d672331ddc6748b8ff
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-12-28 15:53:30 +0100

    nvim: rework null-ls

--- nvim/.config/nvim/lua/knth/plugins/null_ls.lua
+++ nvim/.config/nvim/lua/knth/plugins/null_ls.lua
@@ -1,28 +1,40 @@
-local null_ls = require("null-ls")
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
+-- code action sources
+local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-  null_ls.builtins.formatting.prettier,
-  null_ls.builtins.formatting.eslint_d,
-  null_ls.builtins.formatting.shfmt,
-  null_ls.builtins.formatting.stylua.with({
-    args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
-  }),
-  null_ls.builtins.formatting.codespell,
+  require("null-ls.helpers").conditional(function(utils)
+    if utils.root_has_file(".eslintrc.js") or utils.root_has_file(".eslintrc") then
+      return formatting.eslint_d
+    else
+      return formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } })
+    end
+  end),
 
-  null_ls.builtins.diagnostics.eslint_d,
-  null_ls.builtins.code_actions.gitsigns,
+  formatting.shfmt,
+  formatting.stylua.with({ args = { "--indent-width", "2", "--indent-type", "Spaces", "-" } }),
+  formatting.codespell,
+
+  diagnostics.eslint_d,
+  code_actions.gitsigns,
 }
-
-require("null-ls").config({
-  sources = sources,
-})
 
 lsp_handlers = require("knth.lsp.handlers")
 
-require("lspconfig")["null-ls"].setup({
+null_ls.setup({
+  debug = false,
+  sources = sources,
+
   on_attach = lsp_handlers.on_attach,
   capabilities = lsp_handlers.capabilities,
 })
-
--- the duration in there is to stop timeouts on massive files
--- vim.cmd("autocmd BufWritePost * lua vim.lsp.buf.formatting_seq_sync(nil, 7500)")
