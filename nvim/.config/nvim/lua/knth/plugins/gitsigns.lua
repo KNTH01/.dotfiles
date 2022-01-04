4059597d3e87afea339afe450686517dc1d0b4c1
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2022-01-04 12:20:00 +0100

    nvim: rework gitsigns config

--- nvim/.config/nvim/lua/knth/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/knth/plugins/gitsigns.lua
@@ -1,12 +1,22 @@
-
 -- Gitsigns
-require("gitsigns").setup({
+local status_ok, gitsigns = pcall(require, "gitsigns")
+if not status_ok then
+  return
+end
+
+gitsigns.setup({
+  -- signs = {
+  --   add = { hl = "GitGutterAdd", text = "+" },
+  --   change = { hl = "GitGutterChange", text = "~" },
+  --   delete = { hl = "GitGutterDelete", text = "_" },
+  --   topdelete = { hl = "GitGutterDelete", text = "‾" },
+  --   changedelete = { hl = "GitGutterChange", text = "~" },
+  -- },
   signs = {
-    add = { hl = "GitGutterAdd", text = "+" },
-    change = { hl = "GitGutterChange", text = "~" },
-    delete = { hl = "GitGutterDelete", text = "_" },
-    topdelete = { hl = "GitGutterDelete", text = "‾" },
-    changedelete = { hl = "GitGutterChange", text = "~" },
+    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
   },
 })
-
