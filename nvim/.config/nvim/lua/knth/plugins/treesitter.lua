7ae4499132fc48d0f61dfe7aa673afbed86bca70
Kevin Nguyen <knth@hey.com>
2022-05-28 01:38:33 +0200

    nvim: fix tree sitter configs

--- nvim/.config/nvim/lua/knth/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/knth/plugins/treesitter.lua
@@ -7,7 +7,6 @@ if not status_ok then
 end
 
 treesitter_configs.setup({
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
 
