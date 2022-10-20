990184d46660532af36c27688663e3abffd8c653
Kevin Nguyen <knth@hey.com>
2022-10-20 15:14:23 +0200

    nvim: update breaking change cmp default capabilities

--- nvim/.config/nvim/lua/knth/lsp/handlers.lua
+++ nvim/.config/nvim/lua/knth/lsp/handlers.lua
@@ -101,8 +101,6 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>be", [[<cmd>EslintFixAll<CR>]], opts)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 M.on_attach = function(client, bufnr)
   -- @TODO: check what this is doing
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
@@ -114,6 +112,6 @@ M.on_attach = function(client, bufnr)
   lsp_highlight_document(client)
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
