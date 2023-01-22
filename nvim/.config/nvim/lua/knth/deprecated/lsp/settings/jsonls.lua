12d8093700ab63850f34746fd538a201a8ee94f8
Kevin Nguyen <knth@hey.com>
2023-01-23 00:01:42 +0100

    nvim: configure jsonls schema

--- nvim/.config/nvim/lua/knth/deprecated/lsp/settings/jsonls.lua
+++ nvim/.config/nvim/lua/knth/deprecated/lsp/settings/jsonls.lua
@@ -169,15 +169,6 @@ local opts = {
       schemas = schemas,
     },
   },
-  setup = {
-    commands = {
-      Format = {
-        function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line "$", 0 })
-        end,
-      },
-    },
-  },
 }
 
 return opts
--- nvim/.config/nvim/lua/knth/plugins/lsp_zero.lua
+++ nvim/.config/nvim/lua/knth/plugins/lsp_zero.lua
@@ -24,19 +24,8 @@ lsp.ensure_installed({
   "tailwindcss",
 })
 
+lsp.configure('jsonls', require("knth.deprecated.lsp.settings.jsonls"))
 
--- don't initialize this language server
--- we will use rust-tools to setup rust_analyzer
-lsp.skip_server_setup({ 'rust_analyzer' })
-
--- the function below will be executed whenever
--- a language server is attached to a buffer
-lsp.on_attach(function(client, bufnr)
-  print('Greetings from on_attach', client, bufnr)
-end)
-
--- pass arguments to a language server
--- see :help lsp-zero.configure()
 lsp.configure('tsserver', {
   on_attach = function(client, bufnr)
     print('hello tsserver')
@@ -48,6 +37,8 @@ lsp.configure('tsserver', {
   }
 })
 
+-- the function below will be executed whenever
+-- a language server is attached to a buffer
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
@@ -73,38 +64,43 @@ lsp.on_attach(function(client, bufnr)
 
 end)
 
+-- don't initialize this language server
+-- we will use rust-tools to setup rust_analyzer
+lsp.skip_server_setup({ 'rust_analyzer' })
+
 -- (Optional) Configure lua language server for neovim
 -- see :help lsp-zero.nvim_workspace()
 lsp.nvim_workspace()
 
 --
 -- setup lsp-zero
---
 lsp.setup()
-------------
-
-vim.diagnostic.config({
-  virtual_text = true,
-})
 
 -- initialize rust_analyzer with rust-tools
 -- see :help lsp-zero.build_options()
 local rust_lsp = lsp.build_options('rust_analyzer', {
   single_file_support = false,
   on_attach = function(client, bufnr)
-    print('hello rust-tools')
+    print('Hello from rust-tools')
   end
 })
-
 require('rust-tools').setup({ server = rust_lsp })
 
 --
--- cmp config
+-- After LSP setup
 --
+
+vim.diagnostic.config({
+  virtual_text = true,
+})
+
+--
+-- cmp config
+
 local cmp = require('cmp')
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-  ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+  ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+  ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
   ['<C-d>'] = cmp.mapping.scroll_docs(5),
   ['<C-u>'] = cmp.mapping.scroll_docs(-5),
   ["<c-y>"] = cmp.mapping(
