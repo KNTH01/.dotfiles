656c31911b90d2d2ad1ba562aba9a1a77e277360
Kevin Nguyen <knth@hey.com>
2023-01-23 00:10:47 +0100

    nvim: rust.lua

--- nvim/.config/nvim/lua/knth/deprecated/lsp/settings/rust.lua
+++ nvim/.config/nvim/lua/knth/deprecated/lsp/settings/rust.lua
@@ -1,81 +1,10 @@
 return {
-  -- rust-tools options
-  tools = {
-    -- on_initialized = function()
-    --   vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-    --     pattern = { "*.rs" },
-    --     callback = function()
-    --       vim.lsp.codelens.refresh()
-    --     end,
-    --   })
-    -- end,
-    inlay_hints = {
-      -- prefix for parameter hints
-      parameter_hints_prefix = " ",
+  single_file_support = false,
 
-      -- prefix for all the other hints (type, chaining)
-      other_hints_prefix = " ",
-
-      --   ---------------------------
-
-      --   -- Only show inlay hints for the current line
-      --   only_current_line = false,
-      --
-      --   -- Event which triggers a refersh of the inlay hints.
-      --   -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
-      --   -- not that this may cause higher CPU usage.
-      --   -- This option is only respected when only_current_line and
-      --   -- autoSetHints both are true.
-      --   only_current_line_autocmd = "CursorHold",
-      --
-      --   -- whether to show parameter hints with the inlay hints or not
-      --   -- default: true
-      --   show_parameter_hints = false,
-      --
-      --   -- whether to show variable name before type hints with the inlay hints or not
-      --   -- default: false
-      --   show_variable_name = false,
-      --
-      --   -- prefix for parameter hints
-      --   -- default: "<-"
-      --   -- parameter_hints_prefix = "<- ",
-      --   parameter_hints_prefix = " ",
-      --
-      --   -- prefix for all the other hints (type, chaining)
-      --   -- default: "=>"
-      --   -- other_hints_prefix = "=> ",
-      --   other_hints_prefix = " ",
-      --
-      --   -- whether to align to the length of the longest line in the file
-      --   max_len_align = false,
-      --
-      --   -- padding from the left if max_len_align is true
-      --   max_len_align_padding = 1,
-      --
-      --   -- whether to align to the extreme right or not
-      --   right_align = false,
-      --
-      --   -- padding from the right if right_align is true
-      --   right_align_padding = 7,
-      --
-      --   -- The color of the hints
-      --   highlight = "Comment",
-    },
-    -- hover_actions = {
-    --   auto_focus = false,
-    --   border = "rounded",
-    --   width = 60,
-    --   -- height = 30,
-    -- },
-  },
-
-  -- all the opts to send to nvim-lspconfig
-  -- these override the defaults set by rust-tools.nvim
-  -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-  -- https://rust-analyzer.github.io/manual.html#features
   server = {
-    -- on_attach = require("knth.lsp.handlers").on_attach,
-    -- capabilities = require("knth.lsp.handlers").capabilities,
+    on_attach = function()
+      print('Hello from rust-tools')
+    end,
 
     settings = {
       ["rust-analyzer"] = {
@@ -88,4 +17,15 @@ return {
       },
     },
   },
+
+  -- rust-tools options
+  tools = {
+    inlay_hints = {
+      -- prefix for parameter hints
+      parameter_hints_prefix = " ",
+      -- prefix for all the other hints (type, chaining)
+      other_hints_prefix = " ",
+    },
+  },
+
 }
