f3f4c934ea2814fb23f7b8b0e16e02ee328ea9ab
Kevin Nguyen <knth@hey.com>
2023-02-03 16:14:45 +0100

    nvim: change theme

--- nvim/.config/nvim/lua/knth/colorscheme.lua
+++ nvim/.config/nvim/lua/knth/colorscheme.lua
@@ -4,6 +4,7 @@
 
 -- Catppuccin
 -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-vim.cmd.colorscheme("catppuccin-latte")
+-- vim.cmd.colorscheme("catppuccin-latte")
+vim.cmd.colorscheme("catppuccin-frappe")
 
 -- do not forget to update lualine theme: `lua/{user}/plugins/lualine.lua`
