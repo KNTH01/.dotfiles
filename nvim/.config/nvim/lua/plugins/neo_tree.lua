24bbbebc486433b908d222141ec9d2b22f5db4c1
Kevin Nguyen <knth@hey.com>
2023-04-11 16:58:34 +0200

    nvim: neo_tree config

--- nvim/.config/nvim/lua/plugins/neo_tree.lua
+++ nvim/.config/nvim/lua/plugins/neo_tree.lua
@@ -23,6 +23,30 @@ return {
       filesystem = {
         bind_to_cwd = false,
         follow_current_file = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+          hide_by_name = {
+            "node_modules"
+          },
+          hide_by_pattern = { -- uses glob style patterns
+            --"*.meta",
+            --"*/src/*/tsconfig.json",
+          },
+          always_show = { -- remains visible even if other settings would normally hide it
+            ".gitignored",
+            ".env",
+            ".env.example",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            --".DS_Store",
+            --"thumbs.db"
+          },
+          never_show_by_pattern = { -- uses glob style patterns
+            --".null-ls_*",
+          },
+        },
       },
       window = {
         mappings = {
