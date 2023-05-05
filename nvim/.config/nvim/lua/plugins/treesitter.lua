c8f3df4f8b13e98ae4d9419226359271799eb626
Kevin Nguyen <knth@hey.com>
2023-05-05 16:06:46 +0200

    nvim: add treesitter context

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -60,9 +60,9 @@ return {
 
           autopairs.setup({
             check_ts = true,
-            enable_check_bracket_line = true,      -- Don't add pairs if it already have a close pairs in same line
+            enable_check_bracket_line = true,                -- Don't add pairs if it already have a close pairs in same line
             disable_filetype = { "TelescopePrompt", "vim" }, --
-            enable_afterquote = false,             -- add bracket pairs after quote
+            enable_afterquote = false,                       -- add bracket pairs after quote
             enable_moveright = true,
           })
 
@@ -116,7 +116,26 @@ return {
           })
         end
       },
+
     }
   },
   "nvim-treesitter/playground",
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require 'treesitter-context'.setup {
+        enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+        trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+      }
+    end
+  }
 }
