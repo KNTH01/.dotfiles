06623510b3d2828b9608f811cdd00a33f065bd9e
Kevin Nguyen <knth@hey.com>
2023-05-06 16:57:56 +0200

    nvim: remove icon display in bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -14,12 +14,13 @@ return {
     bufferline.setup({
       highlights = require("catppuccin.groups.integrations.bufferline").get(),
       options = {
-        numbers = "none",                    -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        close_command = "Bdelete! %d",       -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-        left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-        middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
-        separator_style = "slope",           -- | "thick" | "thin" | "slant" | "slope" :h bufferline-styling
+        numbers              = "none",        -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+        close_command        = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+        right_mouse_command  = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+        left_mouse_command   = "buffer %d",   -- can be a string | function, see "Mouse actions"
+        middle_mouse_command = nil,           -- can be a string | function, see "Mouse actions"
+        separator_style      = "slope",       -- | "thick" | "thin" | "slant" | "slope" :h bufferline-styling
+        show_buffer_icons    = false
       },
     })
   end,
