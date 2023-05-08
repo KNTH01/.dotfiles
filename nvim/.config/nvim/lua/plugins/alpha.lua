eb987c8c09694a433086edddb4cda4034417cb09
Kevin Nguyen <knth@hey.com>
2023-05-08 16:49:48 +0200

    nvim: alpha

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -3,46 +3,45 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   event = "VimEnter",
   config = function()
-    local status_ok, alpha = pcall(require, "alpha")
-    if not status_ok then
-      return
-    end
-
     local dashboard = require("alpha.themes.dashboard")
-    dashboard.section.header.val = {
+    local logo = [[
+ _   _                 _
+| \ | |               (_)
+|  \| | ___  _____   ___ _ __ ___
+| . ` |/ _ \/ _ \ \ / / | '_ ` _ \
+| |\  |  __/ (_) \ V /| | | | | | |
+\_| \_/\___|\___/ \_/ |_|_| |_| |_|
 
-      [[          ▀████▀▄▄              ▄█ ]],
-      [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
-      [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
-      [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
-      [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
-      [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
-      [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
-      [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
-      [[   █   █  █      ▄▄           ▄▀   ]],
 
-    }
+     _   __ _   _ _____ _   _
+    | | / /| \ | |_   _| | | |
+    | |/ / |  \| | | | | |_| |
+    |    \ | . ` | | | |  _  |
+    | |\  \| |\  | | | | | | |
+    \_| \_/\_| \_/ \_/ \_| |_/
+]]
 
+
+    dashboard.section.header.val = vim.split(logo, "\n")
     dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
-      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("l", " " .. " Lazy", ":Lazy<CR>"),
+      dashboard.button("q", " " .. " Quit", ":qa<CR>"),
     }
-
-    local function footer()
-      return "Don't Stop Until You are Proud..."
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
     end
 
-    dashboard.section.footer.val = footer()
+    dashboard.section.header.opts.hl = "AlphaHeader"
+    dashboard.section.buttons.opts.hl = "AlphaButtons"
+    dashboard.section.footer.opts.hl = "AlphaFooter"
+    dashboard.opts.layout[1].val = 8
 
-    dashboard.section.footer.opts.hl = "Type"
-    dashboard.section.header.opts.hl = "Include"
-    dashboard.section.buttons.opts.hl = "Keyword"
-
-    dashboard.opts.opts.noautocmd = true
-    alpha.setup(dashboard.opts)
+    require("alpha").setup(dashboard.opts)
   end,
 }
