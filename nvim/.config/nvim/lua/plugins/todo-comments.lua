8956bafcf13f53783cbf2125e793659865490d66
Kevin Nguyen <knth@hey.com>
2023-08-04 13:15:48 +0200

    nvim: add todo-comments and which-keys

---
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    -- https://github.com/folke/todo-comments.nvim
+  }
+}
