1ecd5f193dd793655b13d2633099bba200651e1d
Kevin Nguyen <knth@hey.com>
2023-08-05 14:15:16 +0200

    nvim: add todo-comments

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,9 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Open TODOs list in Telescope" },
+  },
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
