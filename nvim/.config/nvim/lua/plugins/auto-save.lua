31db98fc962305281a025056a3287b17cc29cbf7
Kevin Nguyen <knth@hey.com>
2023-08-05 14:14:08 +0200

    nvim: do not display autosave notifications

---
+++ nvim/.config/nvim/lua/plugins/auto-save.lua
@@ -0,0 +1,16 @@
+return {
+  -- AutoSave
+  "pocco81/auto-save.nvim",
+  config = function()
+    vim.api.nvim_set_keymap("n", "<leader>A", ":ASToggle<CR>", {})
+    -- require('auto-save').setup({
+    --   execution_message = {
+    --     message = function() -- message to print on save
+    --       -- return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+    --       -- disable auto-save message
+    --       return "toto"
+    --     end,
+    --   },
+    -- })
+  end
+}
--- nvim/.config/nvim/lua/plugins/autosave-nvim.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- AutoSave
-  "Pocco81/AutoSave.nvim",
-  config = function()
-    vim.api.nvim_set_keymap("n", "<leader>A", ":ASToggle<CR>", {})
-  end
-}
