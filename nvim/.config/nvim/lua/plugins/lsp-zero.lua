6c629b27f46e0a3833700395056ad8526bd7b742
Kevin Nguyen <knth@hey.com>
2023-09-18 17:49:21 +0200

    nvim: update lsp-zero

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -2,7 +2,7 @@ return
 {
   {
     "VonHeikemen/lsp-zero.nvim",
-    branch = "v1.x",
+    branch = "v3.x",
     dependencies = {
       -- LSP Support
       { "neovim/nvim-lspconfig" },
@@ -30,48 +30,14 @@ return
     config = function()
       -- Learn the keybindings, see :help lsp-zero-keybindings
       -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-      local lsp = require('lsp-zero')
+      local lsp_zero = require('lsp-zero')
 
       -- use cmp setup instead of lsp.setup_nvim_cmp
-      lsp.preset('lsp-compe')
-
-      -- make sure this servers are installed
-      -- see :help lsp-zero.ensure_installed()
-      lsp.ensure_installed({
-        -- rust
-        "rust_analyzer",
-
-        -- lua
-        -- "sumneko_lua",
-
-        -- web dev
-        "tsserver",
-        "jsonls",
-        "volar",
-        "html",
-        "cssls",
-        "eslint",
-        "tailwindcss",
-      })
-
-      -- configure jsonls
-      lsp.configure('jsonls', require("knth.lsp_settings.jsonls"))
-
-      lsp.setup_servers({
-        'volar',
-        'tsserver',
-        opts = {
-          on_init = function(client)
-            -- Format using Prettier
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentFormattingRangeProvider = false
-          end,
-        }
-      })
+      lsp_zero.preset('lsp-compe')
 
       -- the function below will be executed whenever
       -- a language server is attached to a buffer
-      lsp.on_attach(function(client, bufnr)
+      lsp_zero.on_attach(function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
 
         -- LSP actions
@@ -95,17 +61,10 @@ return
         vim.keymap.set("n", "<leader>be", '<cmd>EslintFixAll<cr>', opts)
       end)
 
-      -- don't initialize this language server
-      -- we will use rust-tools to setup rust_analyzer
-      lsp.skip_server_setup({ 'rust_analyzer' })
-
-      -- (Optional) Configure lua language server for neovim
-      -- see :help lsp-zero.nvim_workspace()
-      lsp.nvim_workspace()
 
       --
       -- setup lsp-zero
-      lsp.setup()
+      lsp_zero.setup()
 
       -- initialize rust_analyzer with rust-tools
       require('rust-tools').setup(require("knth.lsp_settings.rust"))
@@ -119,11 +78,12 @@ return
         virtual_text = true,
       })
 
+
       --
       -- cmp config
       local cmp = require('cmp')
-      local cmp_mappings = lsp.defaults.cmp_mappings({
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+        ["<C-o>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ['<C-d>'] = cmp.mapping.scroll_docs(5),
         ['<C-u>'] = cmp.mapping.scroll_docs(-5),
@@ -134,6 +94,8 @@ return
           }),
           { "i", "c" }
         ),
+        ["<tab>"] = cmp.config.disable,
+
         -- C-e is to toggle complete by default, use this to C-e abort and C-Space complete
         -- ['<C-e>'] = cmp.mapping.abort(),
         --   ["<c-space>"] = cmp.mapping({
@@ -151,15 +113,53 @@ return
         --     end,
         --   }),
       })
+
+      print("he")
       cmp_mappings['<Tab>'] = nil
       cmp_mappings['<S-Tab>'] = nil
       cmp_mappings['<CR>'] = nil
 
 
-      local default_sources = { { name = "crates" }, unpack(lsp.defaults.cmp_config().sources) } -- kind of unshift
+      --- LuaSnip
+      local ls = require('luasnip')
+      local types = require "luasnip.util.types"
+
+      ls.config.set_config {
+        -- This tells LuaSnip to remember to keep around the last snippet.
+        -- You can jump back into it even if you move outside of the selection
+        history = false,
+
+        -- This one is cool cause if you have dynamic snippets, it updates as you type!
+        updateevents = "TextChanged,TextChangedI",
+
+        -- Autosnippets:
+        enable_autosnippets = true,
+
+        -- Crazy highlights!!
+        -- #vid3
+        -- ext_opts = nil,
+        ext_opts = {
+          [types.choiceNode] = {
+            active = {
+              virt_text = { { " « ", "NonTest" } },
+            },
+          },
+        },
+      }
+
+      -- vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+      -- vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+      -- vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+      -- vim.keymap.set({ "i", "s" }, "<C-E>", function()
+      --   if ls.choice_active() then
+      --     ls.change_choice(1)
+      --   end
+      -- end, { silent = true })
+
+      --- END luasnip
 
       cmp.setup(
-        lsp.defaults.cmp_config({
+        lsp_zero.defaults.cmp_config({
           mapping = cmp_mappings,
           experimental = {
             ghost_text = true,
@@ -169,10 +169,59 @@ return
           -- unshift `crates` in the sources
           sources = {
             { name = "crates" },
-            unpack(lsp.defaults.cmp_config().sources)
+            unpack(lsp_zero.defaults.cmp_config().sources)
           }
         })
       )
+
+
+      --
+      -- Configure Mason
+      --
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+
+
+        -- -- configure jsonls
+        -- lsp.configure('jsonls', require("knth.lsp_settings.jsonls"))
+        --
+        -- lsp.setup_servers({
+        --   'volar',
+        --   'tsserver',
+        --   opts = {
+        --     on_init = function(client)
+        --       -- Format using Prettier
+        --       client.server_capabilities.documentFormattingProvider = false
+        --       client.server_capabilities.documentFormattingRangeProvider = false
+        --     end,
+        --   }
+        -- })
+
+        -- Replace the language servers listed here
+        -- with the ones you want to install
+        ensure_installed = {
+
+          -- rust
+          "rust_analyzer",
+
+          -- lua
+          -- "sumneko_lua",
+
+          -- web dev
+          "tsserver",
+          "jsonls",
+          "volar",
+          "html",
+          "cssls",
+          "eslint",
+          "tailwindcss",
+
+        },
+        handlers = {
+          lsp_zero.default_setup,
+          rust_analyzer = lsp_zero.noop,
+        },
+      })
 
       --
       -- Configure Prettier to format code
@@ -195,7 +244,7 @@ return
 
       require('mason-nvim-dap').setup({
         ensure_installed = {
-          "codelldb",
+          -- "codelldb",
           "js-debug-adapter"
         }
       })
@@ -205,3 +254,4 @@ return
   -- Rust LSP
   "simrat39/rust-tools.nvim",
 }
+
