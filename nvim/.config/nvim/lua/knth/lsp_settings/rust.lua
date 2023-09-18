810174195887a452210d499c83ba06f62a037440
Kevin Nguyen <knth@hey.com>
2023-09-19 01:52:36 +0200

    nvim: _on_attach

--- nvim/.config/nvim/lua/knth/lsp_settings/rust.lua
+++ nvim/.config/nvim/lua/knth/lsp_settings/rust.lua
@@ -10,46 +10,50 @@ local rt = require("rust-tools")
 -- rt_codelldb_adapter.port = 13000
 -- then i got: Couldn't connect to 127.0.0.1:13000: ECONNREFUSED lol
 
+
+
 return {
-	server = {
-		-- standalone file support ; setting it to false may improve startup time
-		standalone = true,
+  server = {
+    -- standalone file support ; setting it to false may improve startup time
+    standalone = true,
 
-		on_attach = function(_, bufnr)
-			vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-			vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
-			vim.keymap.set("n", "J", rt.join_lines.join_lines, { buffer = bufnr })
+    on_attach = function(client, bufnr)
+      require("knth.lsp_settings._on_attach").on_attach(client, bufnr)
 
-			vim.keymap.set("n", "<leader>ie", rt.inlay_hints.set, { buffer = bufnr })
-			vim.keymap.set("n", "<leader>id", rt.inlay_hints.unset, { buffer = bufnr })
-		end,
+      vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "J", rt.join_lines.join_lines, { buffer = bufnr })
 
-		settings = {
-			["rust-analyzer"] = {
-				lens = {
-					enable = true,
-				},
-				checkOnSave = {
-					command = "clippy",
-				},
-			},
-		},
-	},
+      vim.keymap.set("n", "<leader>ie", rt.inlay_hints.set, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>id", rt.inlay_hints.unset, { buffer = bufnr })
+    end,
 
-	-- rust-tools options
-	tools = {
-		hover_actions = {
-			auto_focus = true,
-		},
-		inlay_hints = {
-			-- prefix for parameter hints
-			parameter_hints_prefix = " ",
-			-- prefix for all the other hints (type, chaining)
-			other_hints_prefix = " ",
-		},
-	},
+    settings = {
+      ["rust-analyzer"] = {
+        lens = {
+          enable = true,
+        },
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
 
-	dap = {
-		-- adapter = rt_codelldb_adapter
-	},
+  -- rust-tools options
+  tools = {
+    hover_actions = {
+      auto_focus = true,
+    },
+    inlay_hints = {
+      -- prefix for parameter hints
+      parameter_hints_prefix = " ",
+      -- prefix for all the other hints (type, chaining)
+      other_hints_prefix = " ",
+    },
+  },
+
+  dap = {
+    -- adapter = rt_codelldb_adapter
+  },
 }
