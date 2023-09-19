93f2c1d269748a77837690032e34024128b70384
Kevin Nguyen <knth@hey.com>
2023-09-19 21:22:39 +0200

    nvim: load todo-comments

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,13 +1,14 @@
 return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  keys = {
-    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Open TODOs list in Telescope" },
-  },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    -- https://github.com/folke/todo-comments.nvim
-  }
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{ "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Open TODOs list in Telescope" },
+	},
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		-- https://github.com/folke/todo-comments.nvim
+	},
 }
