7cac214ff84ab5e857452c9a7b0cc6675683ca6a
Kevin Nguyen <knth@hey.com>
2023-09-19 21:20:40 +0200

    nvim: configure snippet jumps

--- nvim/.config/nvim/lua/plugins/lsp/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -1,12 +1,21 @@
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		"L3MON4D3/LuaSnip", -- snippet engine
+		{
+			"L3MON4D3/LuaSnip", -- snippet engine
+			version = "2.*",
+		},
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-path", -- source for file system paths
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-nvim-lua",
@@ -29,10 +38,14 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-d>"] = cmp.mapping.scroll_docs(5),
-				["<C-u>"] = cmp.mapping.scroll_docs(-5),
+				["<C-e>"] = function(_)
+					if cmp.visible() then
+						cmp.abort()
+					else
+						cmp.complete()
+					end
+				end,
+
 				["<c-y>"] = cmp.mapping(
 					cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Insert,
@@ -40,29 +53,57 @@ return {
 					}),
 					{ "i", "c" }
 				),
+
+				["<C-j>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<C-k>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<C-l>"] = cmp.mapping(function()
+					if luasnip.choice_active() then
+						luasnip.change_choice(1)
+					end
+				end, { "i" }),
+
+				["<C-d>"] = cmp.mapping.scroll_docs(5),
+				["<C-u>"] = cmp.mapping.scroll_docs(-5),
+
 				["<Tab>"] = cmp.config.disable,
 				["<S-Tab>"] = cmp.config.disable,
 				["<CR>"] = cmp.config.disable,
-
-				["<C-e>"] = function(fallback)
-					if cmp.visible() then
-						cmp.abort()
-					else
-						cmp.complete()
-					end
-				end,
 			}),
 
 			-- sources for autocompletion
 			sources = cmp.config.sources({
+				{ name = "crates" },
 				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "path" }, -- file system paths
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
 			}),
 
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
+				-- changing the order of fields so the icon is the first
+				-- fields = { "menu", "abbr", "kind" },
 				format = lspkind.cmp_format({
 					maxwidth = 50,
 					ellipsis_char = "...",
@@ -71,3 +112,30 @@ return {
 		})
 	end,
 }
+
+-- --- LuaSnip
+-- local ls = require("luasnip")
+-- local types = require("luasnip.util.types")
+--
+-- ls.config.set_config({
+-- 	-- This tells LuaSnip to remember to keep around the last snippet.
+-- 	-- You can jump back into it even if you move outside of the selection
+-- 	history = false,
+--
+-- 	-- This one is cool cause if you have dynamic snippets, it updates as you type!
+-- 	updateevents = "TextChanged,TextChangedI",
+--
+-- 	-- Autosnippets:
+-- 	enable_autosnippets = true,
+--
+-- 	-- Crazy highlights!!
+-- 	-- #vid3
+-- 	-- ext_opts = nil,
+-- 	ext_opts = {
+-- 		[types.choiceNode] = {
+-- 			active = {
+-- 				virt_text = { { " « ", "NonTest" } },
+-- 			},
+-- 		},
+-- 	},
+-- })
