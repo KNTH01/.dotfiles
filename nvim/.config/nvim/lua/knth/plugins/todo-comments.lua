ac66dc06338f202b279d87396b4296080a338ad6
Kevin Nguyen <knth@hey.com>
2024-04-07 15:13:52 +0200

    nvim: add keymap todo-comments

--- nvim/.config/nvim/lua/knth/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/knth/plugins/todo-comments.lua
@@ -1,14 +1,21 @@
 return {
-	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	event = { "BufReadPre", "BufNewFile" },
-	keys = {
-		{ "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Open TODOs list in Telescope" },
-	},
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		-- https://github.com/folke/todo-comments.nvim
-	},
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Open TODOs list in Telescope" },
+  },
+  config = function()
+    local todo_comments = require("todo-comments")
+
+    vim.keymap.set("n", "]t", function()
+      todo_comments.jump_next()
+    end, { desc = "Next todo comment" })
+
+    vim.keymap.set("n", "[t", function()
+      todo_comments.jump_prev()
+    end, { desc = "Previous todo comment" })
+
+    todo_comments.setup()
+  end,
 }
