6efd7bf01590c694dba7e82966888fefe3d8a164
Kevin Nguyen <knth@hey.com>
2024-04-23 15:44:37 +0200

    nvim: vim-fugitive, remove keybinds, in favor for neogit

--- nvim/.config/nvim/lua/knth/plugins/vim-fugitive.lua
+++ nvim/.config/nvim/lua/knth/plugins/vim-fugitive.lua
@@ -2,9 +2,9 @@ return
 {
   -- Git commands in nvim
   "tpope/vim-fugitive",
-  config = function()
-    vim.keymap.set("n", "<leader>gs", [[:tab G<cr>]])
-    vim.keymap.set("n", "<leader>gh", [[:diffget //2<cr>]])
-    vim.keymap.set("n", "<leader>gl", [[:diffget //3<cr>]])
-  end
+  -- config = function()
+  --   vim.keymap.set("n", "<leader>gs", [[:tab G<cr>]])
+  --   vim.keymap.set("n", "<leader>gh", [[:diffget //2<cr>]])
+  --   vim.keymap.set("n", "<leader>gl", [[:diffget //3<cr>]])
+  -- end
 }
