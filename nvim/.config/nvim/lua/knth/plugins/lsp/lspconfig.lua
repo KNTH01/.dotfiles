45e12a3a4c0886f8a37eeabc2350832591cd3f4c
Kevin Nguyen <knth@hey.com>
2024-07-11 12:24:47 +0200

    nvim: lookup for typescript in node module

--- nvim/.config/nvim/lua/knth/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/knth/plugins/lsp/lspconfig.lua
@@ -87,9 +87,9 @@ return {
 							client.server_capabilities.documentFormattingProvider = false
 							client.server_capabilities.documentFormattingRangeProvider = false
 						end,
-						--     on_new_config = function(new_config, new_root_dir)
-						--       new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-						--     end,
+						on_new_config = function(new_config, new_root_dir)
+							new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+						end,
 					})
 				end,
 
