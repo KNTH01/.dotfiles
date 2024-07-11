1a42bb2292c898165da80e410b1a29e0ffa6274c
Kevin Nguyen <knth@hey.com>
2024-07-11 12:24:24 +0200

    nvim: add mason for eslint and formatters

--- nvim/.config/nvim/lua/knth/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/knth/plugins/lsp/mason.lua
@@ -2,13 +2,15 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     -- import mason
     local mason = require("mason")
 
-    -- import mason-lspconfig
+    -- import mason-lspconfig and tool-installer
     local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
     mason.setup({
@@ -42,6 +44,14 @@ return {
 
         -- lua
         "lua_ls",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua",   -- lua formatter
+        "eslint_d", -- js linter
       },
     })
   end,
