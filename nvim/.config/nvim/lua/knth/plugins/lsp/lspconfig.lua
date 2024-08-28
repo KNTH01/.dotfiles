b2d09a9f5d3832107d10db8b1bb642bce2cbe9d1
Kevin Nguyen <knth@hey.com>
2024-08-28 12:03:50 +0200

    nvim: add ts config for inlay

--- nvim/.config/nvim/lua/knth/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/knth/plugins/lsp/lspconfig.lua
@@ -101,6 +101,28 @@ return {
 								},
 							}
 						end,
+						settings = {
+							typescript = {
+								inlayHints = {
+									enumMemberValues = {
+										enabled = true,
+									},
+									functionLikeReturnTypes = {
+										enabled = true,
+									},
+									propertyDeclarationTypes = {
+										enabled = true,
+									},
+									parameterTypes = {
+										enabled = true,
+										suppressWhenArgumentMatchesName = true,
+									},
+									variableTypes = {
+										enabled = true,
+									},
+								},
+							},
+						},
 					})
 				end,
 
