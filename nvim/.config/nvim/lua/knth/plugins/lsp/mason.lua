3c2288c569680d5d294522ff4b579ef8e8147cb7
Kevin Nguyen <knth@hey.com>
2024-09-04 12:57:35 +0200

    nvim: format + lock volar mason

--- nvim/.config/nvim/lua/knth/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/knth/plugins/lsp/mason.lua
@@ -1,58 +1,59 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig and tool-installer
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+		-- import mason-lspconfig and tool-installer
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        -- web dev
-        "html",
-        "tsserver",
-        "cssls",
-        "jsonls",
-        "emmet_ls",
-        "tailwindcss",
-        "graphql",
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				-- web dev
+				"html",
+				"tsserver",
+				"cssls",
+				"jsonls",
+				"emmet_ls",
+				"tailwindcss",
+				"graphql",
 
-        -- vue & astro
-        "volar",
-        "astro",
+				-- vue & astro
+				"volar@1.8.27",
+				-- "volar",
+				"astro",
 
-        -- nix
-        -- "rnix",
+				-- nix
+				-- "rnix",
 
-        -- lua
-        "lua_ls",
-      },
-    })
+				-- lua
+				"lua_ls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua",   -- lua formatter
-        "eslint_d", -- js linter
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"eslint_d", -- js linter
+			},
+		})
+	end,
 }
