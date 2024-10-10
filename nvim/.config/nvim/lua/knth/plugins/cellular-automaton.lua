64b90e0ce1dbc69d8c7459f23a73a1fd1951f809
Kevin Nguyen <knth@hey.com>
2024-10-10 16:23:44 +0200

    nvim: keymap cellular-automaton

--- nvim/.config/nvim/lua/knth/plugins/cellular-automaton.lua
+++ nvim/.config/nvim/lua/knth/plugins/cellular-automaton.lua
@@ -1,4 +1,9 @@
 return {
 	"eandrju/cellular-automaton.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+    vim.keymap.set("n", "<leader>clg", "<cmd>CellularAutomaton game_of_life<CR>")
+		vim.keymap.set("n", "<leader>clr", "<cmd>CellularAutomaton make_it_rain<CR>")
+		vim.keymap.set("n", "<leader>cls", "<cmd>CellularAutomaton scramble<CR>")
+	end,
 }
