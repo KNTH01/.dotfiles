4878909df1d981fd7db43773f7475f7fa7fec1b6
Kevin Nguyen <knth@hey.com>
2024-11-15 11:43:06 +0100

    nvim: harpoon2!

--- nvim/.config/nvim/lua/knth/plugins/auto-save.lua
+++ nvim/.config/nvim/lua/knth/plugins/auto-save.lua
@@ -1,16 +1,29 @@
 return {
-  -- AutoSave
-  "pocco81/auto-save.nvim",
-  config = function()
-    vim.api.nvim_set_keymap("n", "<leader>A", ":ASToggle<CR>", {})
-    -- require('auto-save').setup({
-    --   execution_message = {
-    --     message = function() -- message to print on save
-    --       -- return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-    --       -- disable auto-save message
-    --       return "toto"
-    --     end,
-    --   },
-    -- })
-  end
+	-- AutoSave
+	"pocco81/auto-save.nvim",
+	config = function()
+		vim.api.nvim_set_keymap("n", "<leader>A", ":ASToggle<CR>", {})
+		require("auto-save").setup({
+			condition = function(buf)
+				local excluded_filetypes = { "harpoon" }
+				local filetype = vim.bo[buf].filetype
+
+				for _, ft in ipairs(excluded_filetypes) do
+					if ft == filetype then
+						return false
+					end
+				end
+
+				return true
+			end,
+
+			-- execution_message = {
+			-- 	message = function() -- message to print on save
+			-- 		-- return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+			-- 		-- disable auto-save message
+			-- 		return "toto"
+			-- 	end,
+			-- },
+		})
+	end,
 }
---
+++ nvim/.config/nvim/lua/knth/plugins/harpoon.lua
@@ -0,0 +1,45 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+
+	config = function()
+		local harpoon = require("harpoon")
+
+		-- REQUIRED
+		harpoon:setup({})
+		-- REQUIRED
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Harpoon [a]dd" })
+
+		vim.keymap.set("n", "<leader>j", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Harpoon List" })
+
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end, { desc = "Harpoon to file 1" })
+
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end, { desc = "Harpoon to file 2" })
+
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon to file 3" })
+
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end, { desc = "Harpoon to file 4" })
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		-- vim.keymap.set("n", "<leader>i", function()
+		-- 	harpoon:list():prev()
+		-- end)
+		-- vim.keymap.set("n", "<leader>o", function()
+		-- 	harpoon:list():next()
+		-- end)
+	end,
+}
