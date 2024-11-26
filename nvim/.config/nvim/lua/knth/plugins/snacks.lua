94b50404c236d469ee2aa42391d8002dc5baf22e
Kevin Nguyen <knth@hey.com>
2024-11-26 10:23:31 +0100

    nvim: add snacks

---
+++ nvim/.config/nvim/lua/knth/plugins/snacks.lua
@@ -0,0 +1,196 @@
+---@class snacks.dashboard.Config
+local opts_dashboard = {
+	enabled = false,
+	sections = {
+		{ section = "header" },
+		{ section = "keys", gap = 1, padding = 1 },
+		{ section = "startup" },
+		{
+			section = "terminal",
+			cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+			random = 10,
+			pane = 2,
+			indent = 4,
+			height = 30,
+		},
+	},
+}
+
+return {
+	"folke/snacks.nvim",
+	enabled = true,
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+
+		dashboard = opts_dashboard,
+
+		---@class snacks.bigfile.Config
+		bigfile = {
+			notify = true, -- show notification when big file detected
+			size = 1.5 * 1024 * 1024, -- 1.5MB
+			-- Enable or disable features when big file detected
+			---@param ctx {buf: number, ft:string}
+			setup = function(ctx)
+				vim.b.minianimate_disable = true
+				vim.schedule(function()
+					vim.bo[ctx.buf].syntax = ctx.ft
+				end)
+			end,
+		},
+
+		notifier = {
+			enabled = true,
+			timeout = 3000,
+		},
+
+		quickfile = { enabled = true },
+
+		statuscolumn = { enabled = true },
+
+		words = { enabled = true },
+
+		styles = {
+			notification = {
+				wo = { wrap = true }, -- Wrap notifications
+			},
+		},
+	},
+
+	keys = {
+		{
+			"<leader>un",
+			function()
+				Snacks.notifier.hide()
+			end,
+			desc = "Dismiss All Notifications",
+		},
+		{
+			"<leader>x",
+			function()
+				Snacks.bufdelete()
+			end,
+			desc = "Delete Buffer",
+		},
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>gb",
+			function()
+				Snacks.git.blame_line()
+			end,
+			desc = "Git Blame Line",
+		},
+		{
+			"<leader>gB",
+			function()
+				Snacks.gitbrowse()
+			end,
+			desc = "Git Browse",
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.lazygit.log_file()
+			end,
+			desc = "Lazygit Current File History",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.lazygit.log()
+			end,
+			desc = "Lazygit Log (cwd)",
+		},
+		{
+			"<leader>cR",
+			function()
+				Snacks.rename()
+			end,
+			desc = "Rename File",
+		},
+		{
+			"<c-/>",
+			function()
+				Snacks.terminal()
+			end,
+			desc = "Toggle Terminal",
+		},
+		{
+			"<c-_>",
+			function()
+				Snacks.terminal()
+			end,
+			desc = "which_key_ignore",
+		},
+		{
+			"]]",
+			function()
+				Snacks.words.jump(vim.v.count1)
+			end,
+			desc = "Next Reference",
+		},
+		{
+			"[[",
+			function()
+				Snacks.words.jump(-vim.v.count1)
+			end,
+			desc = "Prev Reference",
+		},
+		{
+			"<leader>N",
+			desc = "Neovim News",
+			function()
+				Snacks.win({
+					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+					width = 0.6,
+					height = 0.6,
+					wo = {
+						spell = false,
+						wrap = false,
+						signcolumn = "yes",
+						statuscolumn = " ",
+						conceallevel = 3,
+					},
+				})
+			end,
+		},
+	},
+
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				-- Setup some globals for debugging (lazy-loaded)
+				_G.dd = function(...)
+					Snacks.debug.inspect(...)
+				end
+				_G.bt = function()
+					Snacks.debug.backtrace()
+				end
+				vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+				-- Create some toggle mappings
+				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+				Snacks.toggle.diagnostics():map("<leader>ud")
+				Snacks.toggle.line_number():map("<leader>ul")
+				Snacks.toggle
+					.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+					:map("<leader>uc")
+				Snacks.toggle.treesitter():map("<leader>uT")
+				Snacks.toggle
+					.option("background", { off = "light", on = "dark", name = "Dark Background" })
+					:map("<leader>ub")
+				Snacks.toggle.inlay_hints():map("<leader>uh")
+			end,
+		})
+	end,
+}
