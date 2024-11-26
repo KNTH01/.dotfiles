469016532469050a88e3956c0d463ff9eb364b01
Kevin Nguyen <knth@hey.com>
2024-11-26 10:30:39 +0100

    nvim: lazygit fixed

--- nvim/.config/nvim/lua/knth/plugins/better-escape.lua
+++ nvim/.config/nvim/lua/knth/plugins/better-escape.lua
@@ -1,14 +1,12 @@
 return {
 	-- Better escape
 	"max397574/better-escape.nvim",
+	enabled = false,
 	config = function()
 		require("better_escape").setup({
 			mappings = {
-				v = {
-					j = {
-						k = false,
-					},
-				},
+				t = { j = { false } }, --lazygit navigation fix
+				v = { j = { k = false } }, -- visual select fix
 			},
 		})
 	end,
