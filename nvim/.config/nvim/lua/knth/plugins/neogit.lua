1f9afec775056ef6cf70b993447b7355eed3e5f3
Kevin Nguyen <knth@hey.com>
2024-12-20 17:21:40 +0100

    nvim: fix neogit config

--- nvim/.config/nvim/lua/knth/plugins/neogit.lua
+++ nvim/.config/nvim/lua/knth/plugins/neogit.lua
@@ -2,11 +2,11 @@ return {
 	"NeogitOrg/neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
-		"sindrets/diffview.nvim", -- optional - Diff integration
+		-- "sindrets/diffview.nvim", -- optional - Diff integration
 
 		-- Only one of these is needed, not both.
 		"nvim-telescope/telescope.nvim", -- optional
-		"ibhagwan/fzf-lua", -- optional
+		-- "ibhagwan/fzf-lua", -- optional
 	},
 	config = function()
 		local neogit = require("neogit")
