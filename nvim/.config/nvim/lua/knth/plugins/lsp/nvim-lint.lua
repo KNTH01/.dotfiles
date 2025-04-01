1eb30c3f9d39e4e32050ff21d48ae7ddac2e428a
Kevin Nguyen <knth@hey.com>
2025-04-01 20:22:09 +0200

    nvim: lint

--- nvim/.config/nvim/lua/knth/plugins/lsp/nvim-lint.lua
+++ nvim/.config/nvim/lua/knth/plugins/lsp/nvim-lint.lua
@@ -14,17 +14,19 @@ return {
 			astro = { "eslint_d" },
 		}
 
+		-- Create autocommand which carries out the actual linting
+		-- on the specified events.
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
-				lint.try_lint()
+				-- Only run the linter in buffers that you can modify in order to
+				-- avoid superfluous noise, notably within the handy LSP pop-ups that
+				-- describe the hovered symbol using Markdown.
+				if vim.opt_local.modifiable:get() then
+					lint.try_lint()
+				end
 			end,
 		})
-
-		-- vim.keymap.set("n", "<leader>l", function()
-		-- 	lint.try_lint()
-		-- end, { desc = "Trigger linting for current file" })
 	end,
 }
