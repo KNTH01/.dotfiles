6717501c69d08725eb87f3e2758f775b9d489069
Kevin Nguyen <knth@hey.com>
2025-04-13 15:31:10 +0200

    nvim: snacks update config with new default

--- nvim/.config/nvim/lua/knth/plugins/snacks.lua
+++ nvim/.config/nvim/lua/knth/plugins/snacks.lua
@@ -18,33 +18,12 @@ return {
 
 	---@type snacks.Config
 	opts = {
-
 		dashboard = opts_dashboard,
-
-		---@class snacks.bigfile.Config
-		bigfile = {
-			enabled = true,
-			notify = true, -- show notification when big file detected
-			size = 1.5 * 1024 * 1024, -- 1.5MB
-			-- Enable or disable features when big file detected
-			---@param ctx {buf: number, ft:string}
-			setup = function(ctx)
-				vim.b.minianimate_disable = true
-				vim.schedule(function()
-					vim.bo[ctx.buf].syntax = ctx.ft
-				end)
-			end,
-		},
-
-		notifier = {
-			enabled = true,
-			timeout = 3000,
-		},
-
+		bigfile = { enabled = true },
+		notifier = { enabled = true },
+		scroll = { enabled = true },
 		quickfile = { enabled = true },
-
 		statuscolumn = { enabled = false },
-
 		words = { enabled = true },
 
 		styles = {
