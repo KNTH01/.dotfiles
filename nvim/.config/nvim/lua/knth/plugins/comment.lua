6cff068d0c1db7eb6a4127efb9ad40799a808d76
Kevin Nguyen <knth@hey.com>
2025-05-02 17:23:04 +0200

    nvim: use ts-comments

--- nvim/.config/nvim/lua/knth/plugins/comment.lua
+++ nvim/.config/nvim/lua/knth/plugins/comment.lua
@@ -1,17 +1,23 @@
 return {
-  -- commenting plugin
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  config = function()
-    local comment = require("Comment")
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-    comment.setup({
-      -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end
+	-- -- commenting plugin
+	-- "numToStr/Comment.nvim",
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- dependencies = {
+	--   "JoosepAlviste/nvim-ts-context-commentstring",
+	-- },
+	-- config = function()
+	--   local comment = require("Comment")
+	--   local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+	--
+	--   comment.setup({
+	--     -- for commenting tsx, jsx, svelte, html files
+	--     pre_hook = ts_context_commentstring.create_pre_hook(),
+	--   })
+	-- end
+	{
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+	},
 }
