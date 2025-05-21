8956054aa20b72dbfaddf6187b3aa9e2c832bd8c
Kevin Nguyen <knth@hey.com>
2025-05-21 19:15:28 +0200

    nvim: verify eslint config

--- nvim/.config/nvim/lua/knth/plugins/lsp/nvim-lint.lua
+++ nvim/.config/nvim/lua/knth/plugins/lsp/nvim-lint.lua
@@ -4,15 +4,42 @@ return {
 	config = function()
 		local lint = require("lint")
 
-		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
-			vue = { "eslint_d" },
-			astro = { "eslint_d" },
-		}
+		local fs = require("vim.fs") -- Using vim.fs for file system operations
+
+		-- Function to check for ESLint config
+		local function find_eslint_config()
+			local current_dir = vim.fn.getcwd()
+			local config_files = {
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.json",
+				".eslintrc.yml",
+				".eslintrc",
+				-- "package.json", -- Check for eslintConfig in package.json
+			}
+
+			for _, file in ipairs(config_files) do
+				if fs.find(file, { path = current_dir, upward = true })[1] then
+					return true
+				end
+			end
+			return false
+		end
+
+		-- Set linters based on ESLint config presence
+		if find_eslint_config() then
+			lint.linters_by_ft = {
+				javascript = { "eslint_d" },
+				typescript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+				svelte = { "eslint_d" },
+				vue = { "eslint_d" },
+				astro = { "eslint_d" },
+			}
+		else
+			lint.linters_by_ft = {}
+		end
 
 		-- Create autocommand which carries out the actual linting
 		-- on the specified events.
