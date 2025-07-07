eef1407e4dae52e39b794ddedec48468256da83b
Kevin Nguyen <knth@hey.com>
2025-07-07 14:33:45 +0200

    nvim: ufo

--- nvim/.config/nvim/lua/knth/plugins/ufo.lua
+++ nvim/.config/nvim/lua/knth/plugins/ufo.lua
@@ -15,7 +15,7 @@ return {
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-		vim.keymap.set("n", "<Tab>", "za", { desc = "Toggle fold" })
+		-- vim.keymap.set("n", "<Tab>", "za", { desc = "Toggle fold" })
 
 		-- Add this to your UFO config for fold preview
 		vim.keymap.set("n", "zK", function()
