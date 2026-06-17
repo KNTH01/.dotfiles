ba3b159b9a73af95c06b0f8e84fc55bba48a2e68
Kevin Nguyen <1632391+KNTH01@users.noreply.github.com>
2026-06-17 19:55:00 +0200

    fix: ctrl+r for atuin, keeps fzf

--- home/dot_config/fish/functions/fish_user_key_bindings.fish
+++ home/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,11 @@ function fish_user_key_bindings
         bind -M $mode \cY accept-autosuggestion
         bind -M $mode \cE complete
     end
+
+    # fzf.fish binds Ctrl+R to its history search by default.
+    # Keep fzf.fish for its other bindings, but let Atuin own history search.
+    if functions -q _atuin_search
+        bind \cr _atuin_search
+        bind -M insert \cr _atuin_search
+    end
 end
